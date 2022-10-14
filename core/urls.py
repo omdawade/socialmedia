f8c927257e7d6e105f390777c7dd3b7741410aba
unknown <omdawade@gmail.com>
2022-10-14 19:31:35 +0530

    commit for new  branch

---
+++ core/urls.py
@@ -0,0 +1,21 @@
+from operator import index
+from django import views
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('settings', views.settings, name='settings'),
+    path('upload', views.upload, name='upload'),
+    path('follow', views.follow, name='follow'),
+    path('search', views.search, name='search'),
+    path('profile/<str:pk>', views.profile, name='profile'),
+    path('like-post', views.like_post, name='like-post'),
+    path('upload', views.upload, name='upload'),
+    path('signup', views.signup, name='signup'),
+    path('signin', views.signin, name='signin'),
+    path('logout', views.logout, name='logout'),
+    
+    
+    
+]
